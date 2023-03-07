c44690ccc73c647d5d54ceeab1cab3f98dfe13db
Erin Millard <contact@erin.id.au>
2023-03-07 15:37:26 +1000

    Add license and committer constants

--- modules/constants/outputs.tf
+++ modules/constants/outputs.tf
@@ -1,3 +1,16 @@
+output "committer" {
+  description = "The committer details to use when committing files to GitHub"
+  value = {
+    name  = "ghalactic-repo-manager[bot]"
+    email = "127176271+ghalactic-repo-manager[bot]@users.noreply.github.com"
+  }
+}
+
+output "license" {
+  description = "The repository license"
+  value       = file("LICENSE")
+}
+
 output "org" {
   description = "The GitHub organization"
   value       = "ghalactic"
