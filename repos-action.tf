9458aa575043310b07d036ac324845bb17d7d63b
Erin Millard <contact@erin.id.au>
2023-03-08 14:42:51 +1000

    Create `github-credential-rotation-action` repo

---
+++ repos-action.tf
@@ -0,0 +1,13 @@
+module "repo_github_credential_rotation_action" {
+  source      = "./modules/repo"
+  name        = "github-credential-rotation-action"
+  description = "A GitHub Action that rotates GitHub tokens for you."
+
+  ci_workflows          = ["node"]
+  dependabot_ecosystems = ["docker", "github-actions", "npm"]
+
+  template = {
+    owner      = module.constants.org
+    repository = module.repo_docker_node_action_template.name
+  }
+}
