4ae6159e75ca2ff69143c758d78b1d0ddbe059fa
Erin Millard <contact@erin.id.au>
2023-05-16 18:30:05 +1000

    Remove Docker-based action template
    Docker-based actions are not a great idea. They can only run on Linux
    GHA workers, and they're much slower to start, and more complex to
    publish.

--- repos-template.tf
+++ repos-template.tf
@@ -8,14 +8,3 @@ module "repo_action_template" {
 
   is_template = true
 }
-
-module "repo_docker_node_action_template" {
-  source      = "./modules/repo"
-  name        = "docker-node-action-template"
-  description = "A template repo for Docker + Node.js GitHub Actions"
-
-  ci_workflows          = ["node"]
-  dependabot_ecosystems = ["docker", "github-actions", "npm"]
-
-  is_template = true
-}
