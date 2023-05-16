8ad91dd47b8a3921bc280dbfd5b42fcc3d2dcff2
Erin Millard <contact@erin.id.au>
2023-05-16 18:34:21 +1000

    Standardize repo description punctuation

--- repos-action.tf
+++ repos-action.tf
@@ -1,7 +1,7 @@
 module "repo_github_credential_rotation_action" {
   source      = "./modules/repo"
   name        = "github-credential-rotation-action"
-  description = "A GitHub Action that rotates GitHub tokens for you."
+  description = "A GitHub Action that rotates GitHub tokens for you"
 
   ci_workflows          = ["node"]
   dependabot_ecosystems = ["docker", "github-actions", "npm"]
