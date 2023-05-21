5f9735d93cc8ccc74d2a2830895815a96ee8cf6d
Erin Millard <contact@erin.id.au>
2023-05-21 21:30:04 +1000

    Import the github-release-from-tag action

--- repos-action.tf
+++ repos-action.tf
@@ -6,3 +6,22 @@ module "repo_github_credential_rotation_action" {
   ci_workflows          = ["node"]
   dependabot_ecosystems = ["docker", "github-actions", "npm"]
 }
+
+module "repo_github_release_from_tag" {
+  source       = "./modules/repo"
+  name         = "github-release-from-tag"
+  description  = "A GitHub Action that creates GitHub Releases from your Git tags"
+  homepage_url = "https://github.com/marketplace/actions/github-release-from-tag"
+
+  topics = [
+    "actions",
+    "github-actions",
+    "publishing",
+    "release",
+    "release-automation",
+  ]
+
+  ci_workflows             = []
+  publish_release_workflow = false
+  dependabot_ecosystems    = ["github-actions", "npm"]
+}
