4bcbef6f54885b7361ca127593195e346571d519
Erin Millard <contact@erin.id.au>
2023-06-03 21:01:37 +1000

    Add dependabot-automation repo (with template)

--- repos-action.tf
+++ repos-action.tf
@@ -19,3 +19,27 @@ module "repo_github_release_from_tag" {
   has_discussions         = true
   has_release_discussions = true
 }
+
+module "repo_dependabot_automation" {
+  source       = "./modules/repo"
+  name         = "dependabot-automation"
+  description  = "A GitHub Action that automates your Dependabot busywork"
+  homepage_url = "https://github.com/marketplace/actions/dependabot-automation"
+
+  topics = [
+    "actions",
+    "github-actions",
+    "dependabot",
+    "automation",
+  ]
+
+  ci_workflows          = ["action"]
+  dependabot_ecosystems = ["github-actions", "npm"]
+
+  has_discussions = true
+
+  template = {
+    owner      = module.constants.org
+    repository = module.repo_action_template.name
+  }
+}
