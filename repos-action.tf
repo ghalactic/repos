2ce4614d886a3558228361697792c723d6dcad04
Erin Millard <contact@erin.id.au>
2023-06-03 14:56:05 +1000

    Remove half-baked action

--- repos-action.tf
+++ repos-action.tf
@@ -1,12 +1,3 @@
-module "repo_github_credential_rotation_action" {
-  source      = "./modules/repo"
-  name        = "github-credential-rotation-action"
-  description = "A GitHub Action that rotates GitHub tokens for you"
-
-  ci_workflows          = ["node"]
-  dependabot_ecosystems = ["docker", "github-actions", "npm"]
-}
-
 module "repo_github_release_from_tag" {
   source       = "./modules/repo"
   name         = "github-release-from-tag"
