95b3f534f7aa7b913108fbfe36644c649057f692
Erin Millard <contact@erin.id.au>
2024-03-10 20:41:37 +1000

    Abandon Dependabot automation action

--- repos-action.tf
+++ repos-action.tf
@@ -19,27 +19,3 @@ module "repo_github_release_from_tag" {
   has_discussions         = true
   has_release_discussions = true
 }
-
-module "repo_dependabot_automation" {
-  source       = "./modules/repo"
-  name         = "dependabot-automation"
-  description  = "A GitHub Action that automates your Dependabot busywork"
-  homepage_url = "https://github.com/marketplace/actions/dependabot-automation"
-
-  topics = [
-    "actions",
-    "github-actions",
-    "dependabot",
-    "automation",
-  ]
-
-  ci_workflows                  = ["action"]
-  renovate_post_upgrade_command = "make regenerate"
-
-  has_discussions = true
-
-  template = {
-    owner      = module.constants.org
-    repository = module.repo_action_template.name
-  }
-}
